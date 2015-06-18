fb093d5d38352d3c0fe314ceb416a52b3dff3132
Sven Baumann <baumann.sv@gmail.com>
2015-06-18 17:06:05 +0200

    create the repro

---
+++ contao/config/services.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * contaoblackforest/contao-easy-extend
+ *
+ * Copyright (C) ContaoBlackForest
+ *
+ * @package   contaoblackforest/contao-easy-extend
+ * @file      services.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2015 owner
+ */
+
